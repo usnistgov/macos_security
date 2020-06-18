d82850a04ce21c9857d5550b3c47e5daf038ca27
Dan Brodjieski <brodjieski@gmail.com>
2020-06-18 12:51:06 -0400

    title and subtitle splits

--- scripts/create_guide.py
+++ scripts/create_guide.py
@@ -71,8 +71,8 @@ with open('../templates/adoc_footer.adoc') as adoc_footer_file:
 header_adoc = adoc_header_template.substitute(
     profile_title = profile_yaml['title'],
     description = profile_yaml['description'],
-    html_title = "macOS 10.15 (Catalina)",
-    html_subtitle = "Security Configuration - Baseline",
+    html_title = profile_yaml['title'].split(':')[0],
+    html_subtitle = profile_yaml['title'].split(':')[1],
     pdf_version = "0.9, 2020-06-17",
     html_version = "0.9, 2020-06-17",
     logo = "nist.png"
