80f5462b97507cbae2f17f241d97fde26e3ea367
Dan Brodjieski <brodjieski@gmail.com>
2021-01-20 13:00:17 -0500

    sync with big_sur

--- scripts/generate_guidance.py
+++ scripts/generate_guidance.py
@@ -267,11 +267,58 @@ class PayloadDict:
         self._addPayload(payload_dict, baseline_name)
 
     def finalizeAndSave(self, output_path):
-        """Perform last modifications and save to an output plist.
+        """Perform last modifications and save to configuration profile.
         """
         plistlib.dump(self.data, output_path)
         print(f"Configuration profile written to {output_path.name}")
 
+    def finalizeAndSavePlist(self, output_path):
+        """Perform last modifications and save to an output plist.
+        """
+        output_file_path = output_path.name
+        preferences_path = os.path.dirname(output_file_path)
+        
+
+        settings_dict = {}
+        for i in self.data['PayloadContent']:
+            if i['PayloadType'] == "com.apple.ManagedClient.preferences":
+                for key, value in i['PayloadContent'].items():
+                    domain=key
+                    preferences_output_file = os.path.join(preferences_path, domain + ".plist")
+                    if not os.path.exists(preferences_output_file):
+                        with open(preferences_output_file, 'w'): pass
+                    with open (preferences_output_file, 'rb') as fp:
+                        try:
+                            settings_dict = plistlib.load(fp)
+                        except:
+                            settings_dict = {}
+                    with open(preferences_output_file, 'wb') as fp:
+                        for setting in value['Forced']:
+                            for key, value in setting['mcx_preference_settings'].items():
+                                settings_dict[key] = value
+                    
+                        #preferences_output_path = open(preferences_output_file, 'wb')
+                        plistlib.dump(settings_dict, fp)
+                        print(f"Settings plist written to {preferences_output_file}")
+                    settings_dict.clear()
+                    try:
+                        os.unlink(output_file_path)
+                    except:
+                        continue
+            else:
+                if os.path.exists(output_file_path):
+                    with open (output_file_path, 'rb') as fp:
+                        try:
+                            settings_dict = plistlib.load(fp)
+                        except:
+                            settings_dict = {}
+                for key,value in i.items():
+                    if not key.startswith("Payload"):
+                        settings_dict[key] = value
+        
+                plistlib.dump(settings_dict, output_path)
+                print(f"Settings plist written to {output_path.name}")
+            
 
 def makeNewUUID():
     return str(uuid4())
@@ -326,6 +373,14 @@ def generate_profiles(baseline_name, build_path, parent_dir, baseline_yaml, sign
                 print("Creation of the directory %s failed" %
                     signed_mobileconfig_output_path)
 
+    settings_plist_output_path = os.path.join(
+        f'{build_path}', 'mobileconfigs', 'preferences')
+    if not (os.path.isdir(settings_plist_output_path)):
+        try:
+            os.makedirs(settings_plist_output_path)
+        except OSError:
+            print("Creation of the directory %s failed" %
+                  settings_plist_output_path)
     # setup lists and dictionaries
     profile_errors = []
     profile_types = {}
@@ -382,12 +437,16 @@ def generate_profiles(baseline_name, build_path, parent_dir, baseline_yaml, sign
         if payload.startswith("."):
             unsigned_mobileconfig_file_path = os.path.join(
                 unsigned_mobileconfig_output_path, "com.apple" + payload + '.mobileconfig')
+            settings_plist_file_path = os.path.join(
+                settings_plist_output_path, "com.apple" + payload + '.plist')
             if signing:
                 signed_mobileconfig_file_path = os.path.join(
                 signed_mobileconfig_output_path, "com.apple" + payload + '.mobileconfig')
         else:
             unsigned_mobileconfig_file_path = os.path.join(
                 unsigned_mobileconfig_output_path, payload + '.mobileconfig')
+            settings_plist_file_path = os.path.join(
+                settings_plist_output_path, payload + '.plist')
             if signing:
                 signed_mobileconfig_file_path = os.path.join(
                 signed_mobileconfig_output_path, payload + '.mobileconfig')
@@ -425,7 +484,9 @@ def generate_profiles(baseline_name, build_path, parent_dir, baseline_yaml, sign
 
         else:
             config_file = open(unsigned_mobileconfig_file_path, "wb")
+            settings_config_file = open(settings_plist_file_path, "wb")
             newProfile.finalizeAndSave(config_file)
+            newProfile.finalizeAndSavePlist(settings_config_file)
             config_file.close()
             
     print(f"""
