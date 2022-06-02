48dae6badd8c7f2ec437ef2fe89841cb7876f16b
Bob Gendler <robert.gendler@nist.gov>
2022-06-02 17:20:13 -0400

    removed generate_oval and SCAP directory

--- SCAP/os.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-OS=$(/usr/bin/awk -F ": " '/os: /{print $2}' ../VERSION.yaml | /usr/bin/tr -d '"')
-
-echo $OS
--- SCAP/version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-VERSION=$(/usr/bin/awk -F ": " '/version/{print $2}' ../VERSION.yaml | /usr/bin/awk '{print $NF}' | /usr/bin/tr -d '"')
-echo $VERSION
