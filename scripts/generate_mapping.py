fee8638cc67ab0020325c7cabc2117e9a77d4613
Allen Golbig <golbiga@gmail.com>
2022-08-06 12:37:27 -0400

    more system_settings changes

--- scripts/generate_mapping.py
+++ scripts/generate_mapping.py
@@ -398,7 +398,7 @@ profile:'''.format(other_header,other_header,version_yaml['os'],version_yaml['ve
 
     if len(system_settings) != 0:
         full_baseline = full_baseline + '''
-  - section: "SystemPreferences"
+  - section: "System Settings"
     rules:'''
         system_settings.sort()
     
