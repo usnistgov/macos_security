5dbf9ee3c300368872bb074db52cb4b72035378c
Dan Brodjieski <brodjieski@gmail.com>
2023-08-31 14:53:11 -0400

    fix[rules]: yaml cleanup from merge

--- scripts/util_stig_merge.py
+++ scripts/util_stig_merge.py
@@ -20,7 +20,10 @@ def str_presenter(dumper, data):
     if data.count('\n') > 0:  # check for multiline string
         return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
     return dumper.represent_scalar('tag:yaml.org,2002:str', data)
+def represent_none(self, _):
+    return self.represent_scalar('tag:yaml.org,2002:null', '')
 
+yaml.add_representer(type(None), represent_none)
 yaml.add_representer(str, str_presenter)
 yaml.representer.SafeRepresenter.add_representer(str, str_presenter)
 
