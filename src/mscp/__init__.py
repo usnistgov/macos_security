629a43566979e509f8bacefc139b75b3610a3785
Stephen Beale <stephen.r.beale@gmail.com>
2025-02-14 20:38:54 -0700

    feat: :sparkles: Add GitHub Actions

--- src/mscp/__init__.py
+++ src/mscp/__init__.py
@@ -2,28 +2,6 @@
 
 __version__ = "2.0.0"
 
-from .classes.baseline import Baseline
-from .classes.macsecurityrule import Macsecurityrule
-from .classes.payload import Payload
-from .cli import main
-from .common_utils.config import config
-from .common_utils.file_handling import (
-    append_text,
-    create_csv,
-    create_plist,
-    create_yaml,
-    make_dir,
-    open_csv,
-    open_file,
-    open_plist,
-    open_yaml,
-    remove_dir,
-    remove_dir_contents,
-    remove_file,
-)
-from .common_utils.run_command import run_command
-from .generate import baseline, checklist, guidance, local_report, mapping
-
 __all__ = [
     "Baseline",
     "Macsecurityrule",
@@ -49,3 +27,25 @@ __all__ = [
     "mapping",
     "main",
 ]
+
+from .classes.baseline import Baseline
+from .classes.macsecurityrule import Macsecurityrule
+from .classes.payload import Payload
+from .cli import main
+from .common_utils.config import config
+from .common_utils.file_handling import (
+    append_text,
+    create_csv,
+    create_plist,
+    create_yaml,
+    make_dir,
+    open_csv,
+    open_file,
+    open_plist,
+    open_yaml,
+    remove_dir,
+    remove_dir_contents,
+    remove_file,
+)
+from .common_utils.run_command import run_command
+from .generate import baseline, checklist, guidance, local_report, mapping
