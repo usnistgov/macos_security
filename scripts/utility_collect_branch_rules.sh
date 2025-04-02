576c6406c7919a29897b007f885587d1bc14645f
Dan Brodjieski <brodjieski@gmail.com>
2025-04-02 16:42:13 -0400

    Tweaks for collection script

--- scripts/utility_collect_branch_rules.sh
+++ scripts/utility_collect_branch_rules.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
+# verify that no unstage changes exist
+
+GIT_STATUS=$(git status --porcelain=v1 2>/dev/null | wc -l)
+
+if [[ ! $GIT_STATUS == 0 ]];then
+    echo "There are unstaged changes here, please resolve before continuing."
+    exit 1
+fi
+
 # branches to check out
 
 branches=("sequoia" "sonoma" "ventura" "monterey" "catalina" "big_sur" "ios_16" "ios_17" "ios_18" "visionos")
+
+# get all of the branches checked out and latest information pulled down
+
 git fetch --all
 for branch in ${branches[@]}; do
     git checkout $branch
     git pull
 done
+
+# return to dev_2.0 branch
+# checkout rules from each branch into _work folder
+
 git checkout dev_2.0
+
 for branch in ${branches[@]}; do
     if [[ ! -d "_work/$branch" ]]; then
         mkdir -p "_work/$branch"
@@ -16,14 +33,15 @@ for branch in ${branches[@]}; do
     git --work-tree=_work/$branch checkout $branch -- rules
 
     git restore --staged .
-
-
-
 done
+
+# rename visionOS folder for simplicity
+
 mv _work/visionos _work/visionos_2.0
 
 # clone apple's device-management repo
 
+# remove any existing version of apple repo and recreate
 if [[ -d "_work/apple" ]]; then
     rm -rf "_work/apple"
 fi
@@ -31,3 +49,5 @@ fi
 mkdir -p "_work/apple"
 
 git clone https://github.com/apple/device-management "_work/apple"
+
+exit 0
