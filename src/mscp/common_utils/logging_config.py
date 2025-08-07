58f063c816cd97f7159d35fed1a7efff8cab5671
Dan Brodjieski <brodjieski@gmail.com>
2025-08-07 15:34:35 -0400

    refactor: default logging and modifier
    passing an integer to verbosity will increase log output

--- src/mscp/common_utils/logging_config.py
+++ src/mscp/common_utils/logging_config.py
@@ -13,8 +13,15 @@ from ..classes.loguruformatter import LoguruFormatter
 from .logger_instance import logger
 
 
-def set_logger(debug: bool = False) -> loguru.Logger:
-    log_level: str = "DEBUG" if debug else "INFO"
+def set_logger(debug: bool = False, verbosity: int = 0) -> loguru.Logger:
+    log_level: str = "ERROR"
+
+    if verbosity == 1:
+        log_level = "WARNING"
+    elif verbosity == 2:
+        log_level = "INFO"
+    elif verbosity > 2 or debug:
+        log_level = "DEBUG"
 
     formatter = LoguruFormatter()
     logger.remove()
