144c97e788595ea11edc7d2d2b2ae7d30ba05653
Gregor Dschung <gregor@chkpnt.de>
2025-08-28 14:51:08 +0200

    Write a consolidated .mobileconfig and .plist file

--- scripts/generate_guidance.py
+++ scripts/generate_guidance.py
@@ -525,6 +525,15 @@ def generate_profiles(
         )
         for error in profile_errors:
             print(error)
+
+    consolidated_profile = PayloadDict(
+        identifier="consolidated." + baseline_name,
+        uuid=False,
+        organization="macOS Security Compliance Project",
+        displayname=f"{baseline_name} settings",
+        description=f"Consolidated configuration settings for {baseline_name}."
+    )
+
     # process the payloads from the yaml file and generate new config profile for each type
     for payload, settings in profile_types.items():
         if payload.startswith("."):
@@ -572,17 +581,18 @@ def generate_profiles(
         if payload == "com.apple.ManagedClient.preferences":
             for item in settings:
                 newProfile.addMCXPayload(item, baseline_name)
+                consolidated_profile.addMCXPayload(item, baseline_name)
         # handle these payloads for array settings
         elif (
             (payload == "com.apple.applicationaccess.new")
             or (payload == "com.apple.systempreferences")
             or (payload == "com.apple.SetupAssistant.managed")
         ):
-            newProfile.addNewPayload(
-                payload, concatenate_payload_settings(settings), baseline_name
-            )
+            newProfile.addNewPayload(payload, concatenate_payload_settings(settings), baseline_name)
+            consolidated_profile.addNewPayload(payload, concatenate_payload_settings(settings), baseline_name)
         else:
             newProfile.addNewPayload(payload, settings, baseline_name)
+            consolidated_profile.addNewPayload(payload, settings, baseline_name)
 
         if signing:
             unsigned_file_path = os.path.join(unsigned_mobileconfig_file_path)
@@ -601,6 +611,19 @@ def generate_profiles(
             newProfile.finalizeAndSave(config_file)
             newProfile.finalizeAndSavePlist(settings_config_file)
             config_file.close()
+
+
+    consolidated_plist_file_path = os.path.join(settings_plist_output_path, f"{baseline_name}.plist")
+    with open(consolidated_plist_file_path, "wb") as consolidated_plist_file:
+        consolidated_profile.finalizeAndSavePlist(consolidated_plist_file)
+
+    consolidated_mobileconfig_file_path = os.path.join(unsigned_mobileconfig_output_path, f"{baseline_name}.mobileconfig")
+    with open(consolidated_mobileconfig_file_path, "wb") as consolidated_mobileconfig_file:
+        consolidated_profile.finalizeAndSave(consolidated_mobileconfig_file)
+
+    if signing:
+        signed_consolidated_mobileconfig_path = os.path.join(signed_mobileconfig_output_path, f"{baseline_name}.mobileconfig")
+        sign_config_profile(consolidated_mobileconfig_file_path, signed_consolidated_mobileconfig_path, hash)
 
     print(
         f"""
