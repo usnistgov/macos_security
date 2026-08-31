a6b27d8732c55edc418e833435399d438736532c
Bob Gendler <robert.gendler@nist.gov>
2026-08-31 13:35:13 -0400

    fix [classes] mobileconfig.py
    Updated to support array of dictionaries

--- src/mscp/classes/mobileconfig.py
+++ src/mscp/classes/mobileconfig.py
@@ -64,13 +64,7 @@ def create_value_element(value: Any) -> etree._Element:
             array = etree.Element("array")
             for item in value:
                 array.append(create_value_element(item))
-            return array
-        # case list():
-        #     array = etree.Element("array")
-        #     for item in value:
-        #         item_elem = etree.SubElement(array, "string")
-        #         item_elem.text = item
-        #     return array
+            return array        
         case dict():
             dict_elem = etree.Element("dict")
             for k, v in value.items():
